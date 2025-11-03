95311ee4f260286ce50c0290ad96e3ccd96e4e60
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-03 18:17:18 +0100

    first commit

---
+++ spring/src/test/java/com/spring_full_stack/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_full_stack.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
