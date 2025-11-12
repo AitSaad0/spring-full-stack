c24de4456c6b5cf340b3847ce75d830c7454b84e
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-12 17:18:59 +0100

    creat a rest api

---
+++ spring/src/main/java/com/spring_full_stack/spring/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.spring_full_stack.spring.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+    @GetMapping
+    public String getString(){
+        return "Hello World";
+    }
+}
