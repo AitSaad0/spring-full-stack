29014e7f27f3f3aed1be601233f286f16abcf3eb
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-12 18:07:46 +0100

    up to date

---
+++ spring/src/main/java/com/spring_full_stack/spring/spring.java
@@ -0,0 +1,14 @@
+package com.spring_full_stack.spring;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class spring {
+    public static void main(String[] args) {
+
+        SpringApplication.run(spring.class, args);
+
+    }
+}
