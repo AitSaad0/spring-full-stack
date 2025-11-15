5bd8c5d9e9cc0b6ca4f71cdaea0c392f1feb0866
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-15 21:29:29 +0100

    up to date

--- spring/src/main/java/com/spring_full_stack/spring/controller/ProductController.java
+++ spring/src/main/java/com/spring_full_stack/spring/controller/ProductController.java
@@ -6,8 +6,11 @@ import com.spring_full_stack.spring.entity.Product;
 import com.spring_full_stack.spring.service.IProductService;
 import com.spring_full_stack.spring.service.impl.ProductServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,9 +23,9 @@ public class ProductController {
     private final IProductService iProductService;
 
     @GetMapping
-    public List<ProductDto> getString(){
+    public ResponseEntity<List<ProductDto>> getString(){
         List<ProductDto> productList = iProductService.getProduct();
         System.out.println("productList");
-        return productList;
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(productList);
     }
 }
