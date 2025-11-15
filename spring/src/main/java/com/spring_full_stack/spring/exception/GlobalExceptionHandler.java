11d8ef2ef9461c8e4a3a0a08d25f59a78448c9cc
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-15 21:51:16 +0100

    handling errors globaly

--- spring/src/main/java/com/spring_full_stack/spring/exception/GlobalExceptionHandler.java
+++ spring/src/main/java/com/spring_full_stack/spring/exception/GlobalExceptionHandler.java
@@ -1,9 +1,20 @@
 package com.spring_full_stack.spring.exception;
 
+import com.spring_full_stack.spring.dto.ErrorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    public
+    public ResponseEntity<ErrorDto> globalExceptionHandler(Exception ex,
+                                                           WebRequest web) {
+        ErrorDto errorDto = new ErrorDto(web.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
+    }
 
 }
