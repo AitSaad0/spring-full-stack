0ce15d699e3c4bf6f2fb73d13a61b7b36da0daa6
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-17 12:54:10 +0100

    add anotations

--- spring/src/main/java/com/spring_full_stack/spring/dto/UserDto.java
+++ spring/src/main/java/com/spring_full_stack/spring/dto/UserDto.java
@@ -1,6 +1,9 @@
 package com.spring_full_stack.spring.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,7 +14,13 @@ import lombok.ToString;
 public class UserDto {
 
     private int userId;
+
+    @NotBlank(message = "Name must not be blank")
+    @Size(max = 30, message = "Name must not be have more than 30 character")
     private String fullName;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "you must enter a valid Email")
     private String email;
 
 }
