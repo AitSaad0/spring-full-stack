05a62242e6642b15181afd0304826ab2ded35a02
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-17 12:54:54 +0100

    handlin not valid exception

--- spring/src/main/java/com/spring_full_stack/spring/exception/GlobalExceptionHandler.java
+++ spring/src/main/java/com/spring_full_stack/spring/exception/GlobalExceptionHandler.java
@@ -3,18 +3,33 @@ package com.spring_full_stack.spring.exception;
 import com.spring_full_stack.spring.dto.ErrorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> globalExceptionHandler(Exception ex,
                                                            WebRequest web) {
         ErrorDto errorDto = new ErrorDto(web.getDescription(false),
                 HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDto);
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        fieldErrors.forEach(fieldError -> errors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
 
+    }
 }
