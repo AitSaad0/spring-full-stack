ce7eca3573c35b3a8d43a7268405da5db9055d71
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-20 15:41:20 +0100

    add public path method

--- spring/src/main/java/com/spring_full_stack/spring/security/FullStackSecurityConfig.java
+++ spring/src/main/java/com/spring_full_stack/spring/security/FullStackSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring_full_stack.spring.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -8,18 +9,49 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class FullStackSecurityConfig {
+
+    private final List<String> publicPaths;
+
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests((requests) -> (
-                requests.requestMatchers("api/v1/products/**")).authenticated()
-                .anyRequest().authenticated())
+        return http.csrf(csrfConfig -> csrfConfig.disable())
+                .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests((requests) -> {
+                    publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
+                    requests.anyRequest().authenticated();
+        }
+        )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 }
---
+++ spring/src/main/java/com/spring_full_stack/spring/security/PublicPathMethod.java
@@ -0,0 +1,18 @@
+package com.spring_full_stack.spring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class PublicPathMethod {
+
+    @Bean
+    public List<String> publicPaths() {
+        return List.of(
+                "/api/v1/products/**"
+        );
+    };
+
+}
