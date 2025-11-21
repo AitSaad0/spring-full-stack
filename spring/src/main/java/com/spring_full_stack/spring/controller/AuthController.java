50dcb1bcbd42ca2a78528a03210c84e1c47b5144
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-21 19:13:05 +0100

    Login request dto

--- spring/src/main/java/com/spring_full_stack/spring/controller/AuthController.java
+++ spring/src/main/java/com/spring_full_stack/spring/controller/AuthController.java
@@ -1,10 +1,12 @@
 package com.spring_full_stack.spring.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-
+@RequiredArgsConstructor
 public class AuthController {
+
 }
---
+++ spring/src/main/java/com/spring_full_stack/spring/dto/LoginRequestDto.java
@@ -0,0 +1,4 @@
+package com.spring_full_stack.spring.dto;
+
+public record LoginRequestDto(String username, String password) {
+}
