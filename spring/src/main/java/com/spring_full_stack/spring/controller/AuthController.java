bcf9e256aa83fb0505db5009074429464560a9db
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-22 18:33:00 +0100

    add register request dto

--- spring/src/main/java/com/spring_full_stack/spring/controller/AuthController.java
+++ spring/src/main/java/com/spring_full_stack/spring/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.spring_full_stack.spring.controller;
 
 import com.spring_full_stack.spring.dto.LoginRequestDto;
 import com.spring_full_stack.spring.dto.LoginResponseDto;
+import com.spring_full_stack.spring.dto.RegisterRequestDto;
 import com.spring_full_stack.spring.dto.UserDto;
 import com.spring_full_stack.spring.util.JwtUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,16 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -24,6 +31,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final InMemoryUserDetailsManager inMemoryUserDetailsManager;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> apiLogin(@RequestBody LoginRequestDto loginRequestDto) {
@@ -47,6 +56,16 @@ public class AuthController {
             return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,
                     "An unexpected error occurred");
         }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> apiRegister(@Valid @RequestBody RegisterRequestDto registerRequestDto) {
+        inMemoryUserDetailsManager.createUser(new User(registerRequestDto.getEmail(),
+                passwordEncoder.encode(registerRequestDto.getPassword()),
+                List.of(new SimpleGrantedAuthority("USER"))));
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body("Registration successful");
     }
 
     private ResponseEntity<LoginResponseDto> buildErrorResponse(HttpStatus status,
